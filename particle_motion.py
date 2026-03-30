d15a641b30ee8afc8be542f86bfdec7c5bc94d40
AdvayRChandra <advay.chandra@protonmail.com>
2026-03-29 22:16:57 -0400

    Implement force and acceleration methods in Particle class

--- particle_motion.py
+++ particle_motion.py
@@ -8,7 +8,26 @@ class Particle(np.ndarray):
                                 ("position", "float32"), 
                                 ("velocity", "float32"), 
                                 ("acceleration", "float32")])
+        self.density = mass / (4/3 * np.pi * rad**3)
 
+    def force_x(self, **kwargs):
+        # Placeholder for force function in x direction
+        v_air = kwargs.get("v_air", 0.0)
+        v = self["velocity"].norm()
+
+        
+        return 0.0
+
+    def force_y(self, **kwargs):
+        # Placeholder for force function in y direction
+        return 0.0
+
+    def acceleration_x(self, **kwargs):
+        return self.force_x(**kwargs) / self["mass"]
+    
+    def acceleration_y(self, **kwargs):
+        return self.force_y(**kwargs) / self["mass"]
+    
     @staticmethod
     def update_particle(particle: np.ndarray, function = None, dt: float = 1.0):
         # Update the velocity based on the provided function
